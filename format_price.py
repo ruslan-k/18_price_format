d7b46bcbe4a106cb6a702fc24bc9cacfdfdaad7f
Ruslan Kurbanov <wkurbanov@gmail.com>
2017-01-05 23:24:28 +0300

    Add get_arguments() function, add code to __main__ section

--- format_price.py
+++ format_price.py
@@ -1,13 +1,25 @@
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Convert price to readable format.')
+    parser.add_argument('price', help='Price to format')
+    args = parser.parse_args()
+
+    return args
+
+
 def convert_string_to_float_or_int(string):
-        try:
-            str_as_num = int(string)
-            return str_as_num
-        except ValueError:
-            try:
-                str_as_num = float(string)
-            except ValueError:
-                raise ValueError('Given value cannot be converted into a number')
+    try:
+        str_as_num = int(string)
         return str_as_num
+    except ValueError:
+        try:
+            str_as_num = float(string)
+        except ValueError:
+            raise ValueError('Given value cannot be converted into a number')
+    return str_as_num
+
 
 def format_price(price):
     if isinstance(price, str):
@@ -18,5 +30,7 @@ def format_price(price):
     elif isinstance(price, float):
         return '{:,.2f}'.format(price).replace(',', ' ')
 
+
 if __name__ == '__main__':
-    pass
+    args = get_arguments()
+    print(format_price(args.price))
